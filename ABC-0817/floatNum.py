451ed0790b242e7c1527ea8c770e3c206db08189
KOYAMA SHOYA <s1f102103189@iniad.org>
2024-08-17 23:04:53 +0900

    0817 add

--- ABC-0817/floatNum.py
+++ ABC-0817/floatNum.py
@@ -1,23 +1,8 @@
-def main():
-    x = input().strip()
+x = input().strip()
 
-    # 末尾の '0' を削除
-    while x.endswith('0'):
-        x = x[:-1]
+while x.endswith('0'):
+  x = x[:-1]
+if x.endswith('.'):
+  x = x[:-1]
 
-    # もし小数点 '.' が残っていたら、それも削除
-    if x.endswith('.'):
-        x = x[:-1]
-
-    print(x)
-
-if __name__ == "__main__":
-    main()
-
-
-# def main():
-#     x = float(input())
-#     print(x)
-
-# if __name__ == "__main__":
-#     main()
+print(x)
