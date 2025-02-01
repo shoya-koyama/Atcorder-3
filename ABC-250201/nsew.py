c0d86d7d50e5b5a71783f1ae677d912a773bd8e1
KOYAMA SHOYA <s1f102103189@iniad.org>
2025-02-01 23:05:03 +0900

    250201

---
+++ ABC-250201/nsew.py
@@ -0,0 +1,19 @@
+D = input()
+
+if D == 'N':
+    print('S')
+if D == 'S':
+    print('N')
+if D == 'W':
+    print('E')
+if D == 'E':
+    print('W')
+if D == 'NE':
+    print('SW')
+if D == 'SW':
+    print('NE')
+if D == 'NW':
+    print('SE')
+if D == 'SE':
+    print('NW')
+    
